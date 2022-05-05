dce420a30984a1113e6ae81f8dffa9d32fe78915
NotStoneSeok <98035984+isaccKim@users.noreply.github.com>
2022-05-05 20:22:57 +0900

    TeamProject 2
    main.c
    manager.c
    manager.h 올렸습니다.

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "manager.h"
+
+int main(){
+    Product p[30];
+    int count = 0, menu;
+    int index = 0;
+    index = count;
+
+    while (1){
+        menu = selectMenu();
+        if (menu == 0)
+            break;
+       if (menu == 1 && count != 0)listProduct(p,index);//메뉴조회
+        
+        else if (menu == 2){
+            count += ceateProduct(&p[index++]);
+        } //메뉴추가
+
+        else if (menu == 3 && count >= 1){
+            int no = selectDataNo(p, count);
+            if (no == 0){
+                printf("=> 취소함\n");
+                continue;
+            }
+        } //수정
+
+        else if (menu == 4 && count >= 1){
+            int deleteOk; //삭제
+            int no = selectDataNo(p, index);
+            
+            if (no == 0){
+                printf("=> 취소됨\n");
+                continue;
+            }
+            printf("정말로 삭제하시겠습니까(삭제 :1)");
+            scanf("%d", &deleteOk);
+            if (deleteOk == 1)count -= deleteProduct(&p[no-1]);
+        }
+            continue;
+    }
+    printf("종료됨!\n");
+    return 0;
+}
---
+++ manager.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "product.h"
+#include <time.h>
+
+
+int selectMenu(); // 데이터를 다룰 때, 원하는 메뉴를 선택하는 함수
+int selectDataNo(Product *p, int count); // 제품 번호를 불러오는 
+int listProduct(Product *p, int count); // 전체 등록된 제품 리스트 출력
+
+
