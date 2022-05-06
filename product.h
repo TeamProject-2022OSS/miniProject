248e51d336b9edac33609e0f0da3253b810afcdd
Y-eseong <21900149@handong.ac.kr>
2022-05-06 12:03:29 +0900

    Product.h

---
+++ product.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include "manager.h"
+
+int createProduct(Product *p);  //하나의 제품 출력 함수
+void readProduct(Product *p);  //제품을 추가하는 함수
+int updateProduct(Product *p);  //제품을 수정하는 함수
+int deleteProduct(Product *p);  //제품을 삭제하는 함수
