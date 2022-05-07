a6538f63e6e9b28fe7d275293e5185cb3b90cc05
NotStoneSeok <98035984+isaccKim@users.noreply.github.com>
2022-05-07 13:24:42 +0900

    Update product.h

--- product.h
+++ product.h
@@ -2,6 +2,16 @@
 #include <string.h>
 #include "manager.h"
 
+
+typedef struct {
+    char contents[100]; ; // 1: 음료 2: 디저트
+    char name[100];
+    int price;
+    int sugar;
+    int coin; // 자판기에 넣은 돈
+    }Product;
+//CRUD
+
 int createProduct(Product *p);  //하나의 제품 출력 함수
 void readProduct(Product *p);  //제품을 추가하는 함수
 int updateProduct(Product *p);  //제품을 수정하는 함수
