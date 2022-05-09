5ccd60e7a7bd53bd12b53a48e9d71a534b719e47
Y-eseong <21900149@handong.ac.kr>
2022-05-09 23:02:28 +0900

    update product.h

--- product.h
+++ product.h
@@ -9,6 +9,7 @@ typedef struct {
     int price;
     int sugar;
     int coin; // 자판기에 넣은 돈
+    //int change; 거스름돈,,?
     }Product;
 //CRUD
 
@@ -16,3 +17,4 @@ int createProduct(Product *p);  //하나의 제품 출력 함수
 void readProduct(Product *p);  //제품을 추가하는 함수
 int updateProduct(Product *p);  //제품을 수정하는 함수
 int deleteProduct(Product *p);  //제품을 삭제하는 함수
+//void menuDrink();   void menuDessert(); 음료 메뉴판과 디저트 메뉴판
